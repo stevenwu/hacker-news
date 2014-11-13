e0f6c3e4c0b65aad5515e01d7f996e8779d67a50
Steven Wu <wusteven@gmail.com>
2014-11-13 10:53:09 -0800

    Don't show deleted items

--- app/models/item.js
+++ app/models/item.js
@@ -12,20 +12,26 @@ export default DS.Model.extend({
   title: DS.attr(),
   url: DS.attr(),
   parent: DS.belongsTo('item', {async: true, inverse: 'kids'}),
-  deleted: DS.attr(),
+  deleted: DS.attr('boolean'),
   dead: DS.attr(),
   parts: DS.attr(),
 
   shortUrl: function() {
     // var pattern = new RegExp('(?:https|http)://(?:www\.)?((?:[a-z1-9-]+\.){1,2}(?:[a-z]{2,10}))/', 'i');
     var pattern = /((?:www\.){0,1}(?:[a-z0-9]+(?:-[a-z0-9]+)*\.)+[a-z]{2,})/
-    var match = this.get('url').match(pattern);
-    if (match) {
-      return match[1];
+    if (this.get('url')) {
+      var match = this.get('url').match(pattern);
+      if (match) {
+        return match[1];
+      }
     }
   }.property('url'),
 
   timestamp: function() {
     return moment.unix(this.get('time')).fromNow();
-  }.property('time')
+  }.property('time'),
+
+  wasDeleted: function() {
+    return this.get('deleted');
+  }.property('deleted')
 });
