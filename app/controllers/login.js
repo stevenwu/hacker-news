c54b723cbd313fca3b2e972b99733877ef03d55f
Steven Wu <wusteven@gmail.com>
2014-11-18 23:50:32 -0800

    Allow user login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,17 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   firebase: new Firebase('https://ember-hacker-news.firebaseio.com'),
+  needs: 'application',
 
   actions: {
     login: function() {
-      var _this = this;
-      this.get('firebase').authWithPassword({
-        email: this.get('email'),
-        password: this.get('password')
+      var self = this;
+      self.get('firebase').authWithPassword({
+        email: self.get('email'),
+        password: self.get('password')
       }, function(error, authData) {
         if (error === null) {
           console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
-          _this.transitionTo('index');
+          self.store.find('account', authData.uid).then(function(response) {
+            console.log('Account found. Logging in...');
+          }, function(error) {
+            console.log('Account not found. Creating new account...');
+            var newAccount = self.store.createRecord('account');
+            newAccount.set('id', error.recordId);
+            newAccount.set('email', self.get('email'));
+            newAccount.save().then(null, function(error) { console.log(error); });
+          });
+          self.set('controllers.application.hasSession', true);
+          self.transitionToRoute('index');
         } else {
           console.log(error);
         }
