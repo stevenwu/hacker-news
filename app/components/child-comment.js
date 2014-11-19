00211203e06ae4b2c71119d83d83c2f79b5e88eb
Steven Wu <wusteven@gmail.com>
2014-11-18 23:53:18 -0800

    Saved followed users to account info

--- app/components/child-comment.js
+++ app/components/child-comment.js
@@ -5,5 +5,30 @@ export default Ember.Component.extend({
   offset: false,
   timestamp: function() {
     return moment.unix(this.get('comment.time')).fromNow();
-  }.property('time')
+  }.property('time'),
+
+  actions: {
+    followUser: function(userId) {
+      var sessionData = sessionStorage['firebase:session::ember-hacker-news'];
+      var authToken = JSON.parse(sessionData)['token'];
+      var uid = JSON.parse(sessionData)['uid'];
+      console.log('Auth token: ' + authToken);
+      console.log('uid: ' + uid);
+
+      var followTarget = userId;
+
+      var record = this.store.find('account', uid).then(addFollowing);
+      function addFollowing(user) {
+        var newList = user.get('following').push(userId);
+        user.save().then(null, function(error) {
+          console.log(error);
+        });
+      }
+
+      console.log('Following ' + userId);
+    },
+    saveComment: function(commentId) {
+      console.log('Comment saved');
+    }
+  }
 });
