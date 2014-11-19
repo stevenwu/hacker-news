4066892bbe34205fcf0a92dd86096fe849ea017a
Steven Wu <wusteven@gmail.com>
2014-11-18 20:53:18 -0800

    Set default values for Account

--- app/models/account.js
+++ app/models/account.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr(),
-  following: DS.attr(),
-  favorites: DS.attr(),
-  pins: DS.attr()
+  following: DS.attr('array', {defaultValue: []}),
+  favorites: DS.attr('array', {defaultValue: []}),
+  pins: DS.attr('array', {defaultValue: []})
 });
