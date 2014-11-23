d2503621f5efb1f50ba32ec25382de14dc88029a
Steven Wu <wusteven@gmail.com>
2014-11-22 19:49:01 -0800

    Wrap firebase auth callback in Ember.run

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -11,22 +11,25 @@ export default Ember.Controller.extend({
         email: self.get('email'),
         password: self.get('password')
       }, function(error, authData) {
-        if (error === null) {
-          console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
-          self.store.find('account', authData.uid).then(function(response) {
-            console.log('Account found. Logging in...');
-          }, function(error) {
-            console.log('Account not found. Creating new account...');
-            var newAccount = self.store.createRecord('account');
-            newAccount.set('id', error.recordId);
-            newAccount.set('email', self.get('email'));
-            newAccount.save().then(null, function(error) { console.log(error); });
-          });
-          self.set('controllers.application.hasSession', true);
-          self.transitionToRoute('index');
-        } else {
-          console.log(error);
-        }
+        Ember.run(function() {
+
+          if (error === null) {
+            console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
+            self.store.find('account', authData.uid).then(function(response) {
+              console.log('Account found. Logging in...');
+            }, function(error) {
+              console.log('Account not found. Creating new account...');
+              var newAccount = self.store.createRecord('account');
+              newAccount.set('id', error.recordId);
+              newAccount.set('email', self.get('email'));
+              newAccount.save().then(null, function(error) { console.log(error); });
+            });
+            self.set('controllers.application.hasSession', true);
+            self.transitionToRoute('index');
+          } else {
+            console.log(error);
+          }
+        });
       },{
         remember: 'sessionOnly'
       });
